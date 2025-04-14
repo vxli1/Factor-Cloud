3ca121f66228f330f0e9a0b80066d06dcf5ee320
Vanessa Li <160981979+vxli1@users.noreply.github.com>
2025-04-14 05:21:35 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 # url, username, password
 
-CLICKHOUSE_URL = 'https://wej3jcde4a.ap-northeast-1.aws.clickhouse.cloud:8443?database=default'
+CLICKHOUSE_URL = 'https://your_registered_url'
 CLICKHOUSE_USERNAME = 'default'
-CLICKHOUSE_PASSWORD = 'g3_28R1E7IFJG'
+CLICKHOUSE_PASSWORD = 'Your Password'
